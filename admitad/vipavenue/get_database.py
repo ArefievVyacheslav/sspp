515a157ee92e44f44c5576a14e9e3ad254b5e662
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-05-29 14:36:44 +0300

    fix vipavenue

--- admitad/vipavenue/get_database.py
+++ admitad/vipavenue/get_database.py
@@ -2,7 +2,6 @@ from pymongo import MongoClient
 
 
 def get_database(db_name):
-    # CONNECTION_STRING = "mongodb://localhost:27017"
-    CONNECTION_STRING = "mongodb://local:Qwerty_123@localhost:27017"
+    CONNECTION_STRING = "mongodb://localhost:27017"
     client = MongoClient(CONNECTION_STRING)
     return client[db_name]

a01f686efc99dcec4fdfbea587afea27853b87ce
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-05-29 18:39:58 +0300

    elyts test

--- admitad/elyts/get_images.py
+++ admitad/elyts/get_images.py
@@ -1,5 +1,5 @@
 def get_images(soup):
     images = []
     img_proto = soup.find_all('span', class_='picture-item')
-    for img in img_proto: images.append('https://elyts.ru' + img.find('img')['src'])
+    for img in img_proto: images.append(img.find('img')['src'])
     return images

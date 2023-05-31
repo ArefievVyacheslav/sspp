e5d681984a19700a30c8e1d319ddefc0b8fbad9b
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2023-05-31 11:28:04 +0300

    fix size shoes stckmn

--- advcake/stockmann/get_sizes.py
+++ advcake/stockmann/get_sizes.py
@@ -9,9 +9,10 @@ def get_sizes(soup, brand, category):
         for class_tag in size['class']:
             if  'size_button_disabled__' in class_tag: disabled = True
         if not disabled:
-            if category == 'Обувь' and (brand == 'ADIDAS' or brand == 'MICHAEL KORS'
-                                        or brand == 'LACOSTE' or brand == 'RALPH LAUREN'
-                                        or brand == 'EMPORIO ARMANI' or brand == 'KEDS' or brand == 'PUMA'):
+            # if category == 'Обувь' and (brand == 'ADIDAS' or brand == 'MICHAEL KORS'
+            #                             or brand == 'LACOSTE' or brand == 'RALPH LAUREN'
+            #                             or brand == 'EMPORIO ARMANI' or brand == 'KEDS' or brand == 'PUMA'):
+            if category == 'Обувь':
                 sizes.append(size.find('div', re.compile('size_main__')).text.replace(',', '.'))
             else: sizes.append(size.find('div', re.compile('size_second__')).text.replace(',', '.'))
     return sizes

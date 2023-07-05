4072cbb33317a0bc8bf7a43ec4c10a9a7a9cf280
Никита Шелепов <shelepovfamily@gmail.com>
2023-07-05 09:17:53 +0500

    temp commit

---
+++ admitad/vipavenue_node/getDeeplinks.js
@@ -0,0 +1,29 @@
+const axios = require('axios')
+const sleep = require('await-sleep')
+
+
+module.exports = async function getDeeplinks (products) {
+
+  let counter = 0
+
+  for (let product of products) {
+
+    try {
+      counter += 1
+
+      const res = await axios.post('http://localhost:3005/deeplink', {
+        'deeplink': product.link,
+        'pp': 'admitad',
+        // 'offerId': 14647
+      })
+      product.link = res.data
+
+      console.log(counter, 'deeplink')
+      await sleep(100)
+    } catch (e) {
+      console.log('Петушиный диплинк не пришёл')
+      products = products.filter(productNew => productNew.link !== product.link)
+    }
+  }
+  return products
+}
